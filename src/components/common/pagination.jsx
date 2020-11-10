a456ab1e3d2e40adc756a59de0da59603548fd70
Ratan Hegde <ratanhegde@pop-os.localdomain>
2020-11-10 10:40:20 +0530

    add pagination

---
+++ src/components/common/pagination.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import _ from "lodash";
+import propTypes from "prop-types";
+
+class Pagination extends Component {
+  render() {
+    const { itemsCount, pageSize, onPageChange, currentPage } = this.props;
+    // console.log(currentPage);
+    const pageCount = Math.ceil(itemsCount / pageSize);
+    if (pageCount === 1) return null;
+    const pages = _.range(1, pageCount + 1);
+
+    return (
+      <nav>
+        <ul className="pagination">
+          {pages.map((page) => (
+            <li
+              key={page}
+              className={
+                page === currentPage ? "page-item active" : "page-item"
+              }
+            >
+              <a className="page-link" onClick={() => onPageChange(page)}>
+                {page}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    );
+  }
+}
+
+Pagination.propTypes = {
+  itemsCount: propTypes.number.isRequired,
+  pageSize: propTypes.number.isRequired,
+  onPageChange: propTypes.func.isRequired,
+  currentPage: propTypes.number.isRequired,
+};
+
+export default Pagination;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import { getMovies, deleteMovie } from "../services/fakeMovieService";
 import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "../utils/paginate";
+import ListGroup from "./common/listGroup";
 
 class Movies extends Component {
-  state = { movies: getMovies() };
+  state = { movies: getMovies(), pageSize: 4, currentPage: 1 };
 
   handleLike = (movie) => {
     const movies = [...this.state.movies];
@@ -14,61 +17,79 @@ class Movies extends Component {
   };
 
   render() {
+    const count = this.state.movies.length;
+    const { pageSize, currentPage, movies: allMovies } = this.state;
+
+    const movies = paginate(allMovies, currentPage, pageSize);
     return (
       <div>
-        {this.state.movies.length === 0 ? (
+        {count === 0 ? (
           <h1>There are no movies</h1>
         ) : (
           <>
-            <h1>There are {this.state.movies.length} movies in the database</h1>
-            <table width="100%" className="table">
-              <thead>
-                <tr>
-                  <th>Title</th>
-                  <th>Genre</th>
-                  <th>In Stock</th>
-                  <th>Rental rate</th>
-                  <th>Like</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.movies.map((movie) => (
-                  <tr>
-                    <td>{movie.title}</td>
-                    <td>{movie.genre.name}</td>
-                    <td>{movie.numberInStock}</td>
-                    <td>{movie.dailyRentalRate}</td>
-                    <td>
-                      <Like
-                        liked={movie.liked}
-                        onClick={() => this.handleLike(movie)}
-                      />
-                    </td>
-                    <td>
-                      <button
-                        className="btn btn-danger"
-                        onClick={() => {
-                          deleteMovie(movie._id);
-                          // handleDeleteMovies();
-                          this.setState({ movies: getMovies() });
-                        }}
-                      >
-                        Delete
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <h1>There are {count} movies in the database</h1>
+            <div className="row">
+              <div className="col-3 mt-4">
+                <ListGroup />
+              </div>
+              <div className="col-9">
+                <table width="100%" className="table">
+                  <thead>
+                    <tr>
+                      <th>Title</th>
+                      <th>Genre</th>
+                      <th>In Stock</th>
+                      <th>Rental rate</th>
+                      <th>Like</th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {movies.map((movie) => (
+                      <tr key={movie._id}>
+                        <td>{movie.title}</td>
+                        <td>{movie.genre.name}</td>
+                        <td>{movie.numberInStock}</td>
+                        <td>{movie.dailyRentalRate}</td>
+                        <td>
+                          <Like
+                            liked={movie.liked}
+                            onClick={() => this.handleLike(movie)}
+                          />
+                        </td>
+                        <td>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => {
+                              deleteMovie(movie._id);
+                              // handleDeleteMovies();
+                              this.setState({ movies: getMovies() });
+                            }}
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <Pagination
+                  itemsCount={count}
+                  pageSize={pageSize}
+                  currentPage={currentPage}
+                  onPageChange={this.handlePageChange}
+                />
+              </div>
+            </div>
           </>
         )}
       </div>
     );
   }
-  handleDeleteMovies() {
-    console.log(this);
-  }
+  handlePageChange = (page) => {
+    const currentPage = page;
+    this.setState({ currentPage });
+  };
 }
 
 export default Movies;
